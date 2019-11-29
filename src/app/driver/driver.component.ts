83e6bb1da2362e61d97858127bdd1a2b79c0efb0
Tobias Klug <tobias.klug@datev.de>
2019-11-29 10:14:56 +0100

    validators and nested

--- src/app/driver/driver.component.ts
+++ src/app/driver/driver.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators, AbstractControl, ValidationErrors, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-driver',
@@ -11,21 +11,52 @@ export class DriverComponent implements OnInit {
   public name: FormControl;
   public vorname: FormControl;
   public driverForm: FormGroup;
+  public address: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    
-    
-    this.name = new FormControl('Müller');
-    this.vorname = new FormControl();
-    
-    this.vorname.setValue('ghp');
-    this.driverForm = new FormGroup({'name': this.name, 'vorname': this.vorname});
+    this.name = new FormControl(null, [Validators.required, Validators.maxLength(10), this.startsWithP, this.hasNumberOfPs(3)]);
+    this.vorname = new FormControl(null, Validators.required);
+    this.address = this.formBuilder.group({street: ['Sesamstrasse'], city: ['Bamberg']})
+    this.driverForm = new FormGroup({'name': this.name, 'vorname': this.vorname, 'address': this.address});
+    this.initForm();
+  }
 
+  hasNumberOfPs(numberOfPs: number) {
+    return (control: AbstractControl) => {
+      const value: string = control.value;
+      if(value && value.split('P').length >= numberOfPs) {
+        return {hasNumberOfPs: {errorCode: 711, description: 'Has >= ' + numberOfPs + ' Ps' }}
+      }
+      else {
+        return null;
+      }
+    }
+  }
+
+  private startsWithP(control: AbstractControl): ValidationErrors | null {
+    const value: string = control.value;
+    if(value && value.substring(0,1) !== 'P'){
+      return {startsWithP: {errorCode: 4711, description: 'starts with p'}};
+    }
+    return null;
+  }
+
+  private initForm(){
+    this.driverForm.reset({'name': 'Müller', 'vorname': 'Peter', 'address': {'street': 'Sesamstrasse', 'city': 'Bamberg'}});
   }
 
   public onSubmit(){
-    console.log('submit', this.driverForm.value );
+    if(this.driverForm.valid){
+      console.log('submit', this.driverForm.value );
+    } else {
+      console.log('fehler');
+      console.log(this.driverForm.controls.name.errors)
+    }
+  }
+
+  public resetForm(){
+    this.initForm();
   }
 }
